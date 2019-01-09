1df78d88386ec26f7a3897d8b93f3f1db67b8806
simpart <simpart>
2019-01-09 04:41:08 +0000

    add close button

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const Button  = require('mofron-comp-button');
 const vsClick = require('mofron-event-visiclick');
 const HrzPos  = require('mofron-effect-hrzpos'); 
 const VrtPos  = require('mofron-effect-vrtpos');
+const SyncHei = require('mofron-effect-synchei');
 
 /**
  * @class mofron.comp.Dialog
@@ -28,6 +29,17 @@ mf.comp.Dialog = class extends mf.Component {
             super();
             this.name('Dialog');
             this.prmMap('title');
+            
+            this.closeButton(
+                new Text({
+                    text: '&times;', mainColor: [120,120,120],
+                    effect: [
+                        new SyncHei(this.frame().header()),
+                        new HrzPos('right', '0.1rem')
+                    ]
+                })
+            );
+            
             this.prmOpt(po);
         } catch (e) {
             console.error(e.stack);
@@ -44,10 +56,16 @@ mf.comp.Dialog = class extends mf.Component {
         try {
             super.initDomConts();
              
-            this.child([this.modal()]);
+            
+            /* set close button */
+            this.frame().header().child([this.closeButton()]);
             
             this.modal().child([this.frame()]);
+            this.child([this.modal(), this.btnWrap()]);
+            
+            /* update target dom */
             this.target(this.frame().target());
+            
             
             //this.style();
             //let btn_ara = new mf.Component({
@@ -90,76 +108,38 @@ mf.comp.Dialog = class extends mf.Component {
         }
     }
     
-    button (prm) {
+    btnWrap (prm) {
         try {
-            if (undefined === prm) {
-                /* getter */
-                return (undefined === this.m_button) ? [] : this.m_button;
+            if (true === mf.func.isComp(prm)) {
+                prm.execOption({ effect: [new VrtPos('bottom')] });
             }
-            /* setter */
-            if (true === Array.isArray(prm)) {
-                for (let pidx in prm) {
-                    this.button(prm[pidx]);
-                }
-                return;
-            }
-            if (true !== mf.func.isInclude(prm, 'Button')) {
-                throw new Error('invalid parameter');
-            }
-            
-            prm.execOption({
-                size       : ['1rem', '0.3rem'],
-                sizeValue  : ['margin-left', (0 !== this.button().length) ? '0.3rem' : '0rem']
-            });
-            if (true === this.autoClose()) {
-                prm.execOption({ event : [ new VisiClick('disable', this) ] });
-            }
-            
-            
-            //this.switchTgt(
-            //    this.btnTgt(),
-            //    (cmp) => {
-            //        try {
-            //            let btn_wid = mf.func.getSizeObj(cmp.target().style('width'));
-            //            cmp.target().style({
-            //                width : mf.func.sizeSum(btn_wid, prm.width()).toString()
-            //            });
-            //            
-            //            if (0 !== cmp.button().length) {
-            //                cmp.target().style({
-            //                    width : mf.func.sizeSum(cmp.target().style('width'),'0.3rem').toString()
-            //                });
-            //            }
-            //            
-            //            cmp.addChild(prm);
-            //        } catch (e) {
-            //            console.error(e.stack);
-            //            throw e;
-            //        }
-            //    }
-            //);
-            //
-            //if (undefined === this.m_button) {
-            //    this.m_button = [];
-            //}
-            //this.m_button.push(prm);
+            return this.innerComp('btnWrap', prm, mf.Component);
         } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
     
+    button (prm) {
+        try { return this.btnWrap().child(prm); } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
     autoClose (prm) {
+        try { return this.member('autoClose', 'boolean', prm, true); } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    closeButton (prm) {
         try {
-            if (undefined === prm) {
-                /* getter */
-                return (undefined === this.m_atclose) ? true : this.m_atclose;
+            if (true === mf.func.isComp(prm)) {
+                prm.execOption({ event: [new vsClick('disable',this)] });
             }
-            /* setter */
-            if ('boolean' !== typeof prm) {
-                throw new Error('invalid parameter');
-            }
-            this.m_atclose = prm;
+            return this.innerComp('closeButton', prm, mf.Component);
         } catch (e) {
             console.error(e.stack);
             throw e;
@@ -177,146 +157,45 @@ mf.comp.Dialog = class extends mf.Component {
                 });
             }
             return ret;
-    //        if (undefined === prm) {
-    //            /* getter */
-    //            if (undefined === this.m_frame) {
-    //                this.frame(
-    //                    new Frame({
-    //                        mainColor : new mf.Color(255,255,255),
-    //                        effect    : [ new HrzPos('center'), new VrtPos('center') ],
-    //                    })
-    //                );
-    //            }
-    //            return this.m_frame;
-    //        }
-    //        /* setter */
-    //        if (true !== mf.func.isInclude(prm, 'Frame')) {
-    //            throw new Error('invalid parameter');
-    //        }
-    //        this.m_frame = prm;
         } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
-    
-    //header (prm) {
-    //    try {
-    //        if (undefined === prm) {
-    //            /* getter */
-    //            if (undefined === this.m_header) {
-    //                this.header(
-    //                    new Header({
-    //                        height   : '0.42rem',
-    //                        bind     : false,
-    ///                        navigate : new Text({
-    //                            text  : '&#x2715;',
-    //                            event : [ new Click('disable', this) ]
-    //                        })
-    //                    })
-    //                );
-    //            }
-    //            return this.m_header;
-    //        }
-    //        /* setter */
-    //        if (true !== mf.func.isInclude(prm, 'Header')) {
-    //            throw new Error('invalid parameter');
-    //        }
-    //        this.m_header = prm;
-    //    } catch (e) {
-    //        console.error(e.stack);
-    //        throw e;
-    //    }
-    //}
     
     modal (prm) {
        try { return this.innerComp('modal', prm, Modal);
-    //       if (undefined === prm) {
-    //           /* getter */
-    //           if (undefined === this.m_mdlfil) {
-    //               this.modalfil(new Modal({}));
-    //           }
-    //           return this.m_mdlfil;
-    //       } 
-    //       /* setter */
-    //       if (true !== mf.func.isInclude(prm, 'ModalFil')) {
-    //           throw new Error('invalid parameter');
-    //       }
-    //       this.m_mdlfil = prm;
         } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
     
-    //contents (prm) {
-    //    try {
-    //        if (undefined === prm) {
-    //            /* getter */
-    //            if (undefined === this.m_conts) {
-    //                this.contents(new mf.Component({ width : '100%' }));
-    //            }
-    //            return this.m_conts;
-    //        }
-    //        /* setter */
-    //        if (true !== mf.func.isInclude(prm, 'Component')) {
-    //            throw new Error('invalid parameter');
-    //        }
-    //        this.m_conts = prm;
-    //    } catch (e) {
-    //        console.error(e.stack);
-    //        throw e;
-    //    }
-    //}
-    
-    //btnTgt (prm) {
-    //    try {
-    //        if (undefined === prm) {
-    //            /* getter */
-    //            if (undefined === this.m_btntgt) {
-    //                throw new Error('not find target');
-    //            }
-    //            return this.m_btntgt;
-    //        }
-    //        /* setter */
-    //        if (true !== mf.func.isInclude(prm, 'Dom')) {
-    //        }
-    //        this.m_btntgt = prm;
-    //    } catch (e) {
-    //        console.error(e.stack);
-    //        throw e;
-    //    }
-    //}
-    
-    //width (prm) {
-    //    try { return this.frame().width(prm); } catch (e) {
-    //        console.error(e.stack);
-    //        throw e;
-    //    }
-    //}
-    
-    height (prm) {
+    height (prm, hdr) {
         try {
-            return this.frame().height(prm);
+            if (undefined !== hdr) { 
+                this.frame().header().height(hdr);
+            }
+            return super.height(prm);
         } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
     
-    //mainColor (prm) {
-    //    try { return this.header().baseColor(prm); } catch (e) {
-    //        console.error(e.stack);
-    //        throw e;
-    //    }
-    //}
+    mainColor (prm) {
+        try { return this.frame().header().baseColor(prm); } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
     
-    //accentColor (prm) {
-    //    try { return this.modalfil().baseColor(prm); } catch (e) {
-    //        console.error(e.stack);
-    //        throw e;
-    //    }
-    //}
+    accentColor (prm) {
+        try { return this.modal().baseColor(prm); } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
 }
 module.exports = mofron.comp.Dialog;
 /* end of file */
